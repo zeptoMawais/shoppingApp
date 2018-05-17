21d9d0766f7b6245232df8d7f4f5344e9e6f2873
awais-nu <awais.nu@outlook.com>
2018-05-17 11:10:50 +0500

    links fixing

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  main(){
+    this.router.navigate(['main']);
+  }
+  pfcui(){
+    this.router.navigate(['productsFromCatagoryUi']);
+  }
+  ssr(){
+    this.router.navigate(['shopSearchResult']);
+  }
+  sp(){
+    this.router.navigate(['shopProfile']);
+  }
+  pv(){
+    this.router.navigate(['productView']);
+  }
+  cv(){
+    this.router.navigate(['cartView']);
+  }
+  cc(){
+    this.router.navigate(['closecart']);
+  }
+  cp(){
+    this.router.navigate(['closepayment']);
+  }
+  buyerProfile(){
+    this.router.navigate(['buyerProfile']);
   }
 
 }
